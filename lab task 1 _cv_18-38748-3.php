165e5bfc75dc1adbcaf21a59cafd265da52af6e3
AFRINA AMIR NIRJONA <102577685+AFRINA5@users.noreply.github.com>
2022-07-19 08:47:13 +0600

    Add files via upload

---
+++ lab task 1 _cv_18-38748-3.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+  <title>CV</title>
+</head>
+<h1><center>Curriculum Vitae</center></h1>
+<h1><center>----------------------------------------------------------</center></h1>
+<hr align ="center" width="15%">
+
+
+<center>
+    <h2><center>----------------------------------------------------------</center></h2>
+<p> Name: Afrina Amir Nirjona<br>
+    Date Of Birth: 21/07/2000<br>
+    Address: Joydebpur,Gazipur<br>
+    E-mail: nirjonamir1@gmail.com@gmail.com<br>
+    Phone No: 01629824770
+</p>
+    <h2><center>----------------------------------------------------------</center></h2>
+    </center>
+
+<body>
+<h2>CAREER OBJECT:</h2>
+Seeking opportunities to work challenging environment, industry. I will give my best effort<br>knowledge to reach the organization's goals and objectives of the computer science field.<br>To obtain fresher level skills and designing information and ready to enable the company<br>to operate as well as effectively your business.
+
+<form>
+    <fieldset>
+<h2>ACADEMIC DETAILS:</h2>
+      <table>
+<tr>
+    <th >Qualification</th>
+    <th >Session</th>
+    <th >Grade</th>
+    <th >Subject</th>
+    <th >Board</th>
+          </tr>
+<tr>
+    <td >SSC</td>
+    <td >2016</td>
+    <td >GPA-5.00</td>
+    <td >Science</td>
+    <td >Joydebpur Gov Girls High School</td>
+    <td >Gazipur</td>
+</tr>
+<tr>
+    <td >HSC</td>
+    <td >2018</td>
+    <td >GPA-4.32</td>
+    <td >Science</td>
+    <td >Gazipur Metropiltan College</td>
+    <td >Dhaka</td>
+</tr>
+<tr>
+    <td >BSc</td>
+    <td >Current Student</td>
+    <td >CGPA-3.00</td>
+    <td >CSE</td>
+    <td >American International University-Bangladesh</td>
+    <td >None</td>
+</tr>
+
+</fieldset>
+</form>
+</table>
+<hr>
+<h2>EXPERIENCE:</h2>
+      Languages:                  C, C++, C#, JAVA, HTML, PYTHON<br>
+      Platforms:                  Windows 8,10 & Mac OS <br>
+      Concepts:                   Networking, Operating Systems<br>
+<hr>
+<h2>ABILITIES:</h2>
+      Computer Skill:             MS Office, Visual Studio, Visual Basic, AutoCad, Matlab<br>
+      Experience:                 More than 3 years
+                               
+<hr>
+<h2>PERSONAL SKILLS:</h2>
+                                  1.Honesty<br>
+                                  2.Team Spirit<br>
+                                  3.Leadership<br>
+                                  4.Accepting Challenges<br>
+                                  5.Project Management
+<hr>  
+<h2>LANGUAGES:</h2>
+                                  English & Bangla
+<hr>                   
+<h2>INTEREST:</h2>
+                                  1.Internet And Technology<br>
+                                  2.Reading Books<br>
+                                  3.Listening to Music<br>
+                                  4.Computer Science<br>
+                                  5.Sport(Cricket, Football, Tennis, Swimming)
+
+
+</body>
+</html>
